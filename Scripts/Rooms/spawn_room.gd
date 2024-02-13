e12796b841d62c774ea46f673b9cf05c4a6136d5
Mathéo LEGER <71370253+matheoleger@users.noreply.github.com>
2024-02-13 22:35:37 +0100

    :world_map: Add spawnroom + speaker (#6)
    * :construction: WIP spawn room
    
    * :world_map: Add spawnroom + speaker
    
    * :hammer: Add signal listener

---
+++ Scripts/Rooms/spawn_room.gd
@@ -0,0 +1,38 @@
+extends Node3D
+
+@onready var left_door := $LeftDoor
+@onready var right_door := $RightDoor
+@onready var speaker := $Speaker
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	left_door.is_good = true
+	right_door.is_good = false
+	speaker.play_audio(0)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func invert_correct_door():
+	left_door.is_good = !left_door.is_good
+	right_door.is_good = !right_door.is_good
+	
+	if left_door.is_good:
+		speaker.play_audio(3)
+
+
+func _on_speaker_audio_finished():
+	if speaker.is_already_played:
+		return
+	
+	if left_door.is_good:
+		speaker.play_audio(1)
+	elif right_door.is_good:
+		speaker.play_audio(2)
+	
+	speaker.is_already_played = true
+
+
+func _on_area_3d_body_entered(body):
+	pass # Replace with function body.
---
+++ Scripts/speaker.gd
@@ -0,0 +1,52 @@
+extends Node3D
+
+signal audio_finished
+
+@export var audio_sources: Array[AudioStream]
+@export var audio_subtitles: Array[String]
+
+@onready var audio_stream_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
+@onready var subtitle: Label = $CanvasLayer/Subtitle
+
+var current_played_audio = 0
+var is_already_played: bool = false
+
+var tween: Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func play_audio(source_index: int):
+	if source_index >= audio_sources.size() or source_index < 0:
+		return
+	
+	current_played_audio = source_index
+	audio_stream_player.stream = audio_sources[source_index]
+	audio_stream_player.play()
+	
+	display_subtitle(source_index)
+
+func display_subtitle(subtitle_index: int):
+	if tween != null:
+		tween.kill()
+	tween = get_tree().create_tween()
+	
+	subtitle.text = audio_subtitles[subtitle_index]
+	
+	tween.tween_property(subtitle, "modulate:a", 1, .5)
+	
+func hidden_subtitle():
+	if tween != null:
+		tween.kill()
+	tween = get_tree().create_tween()
+	
+	tween.tween_property(subtitle, "modulate:a", 0, .5)
+
+func _on_audio_stream_player_3d_finished():
+	hidden_subtitle()
+	audio_finished.emit()
