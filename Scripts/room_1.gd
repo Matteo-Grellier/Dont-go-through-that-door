05813c38af926c3832424f02f5b14a27bb81cece
matheoleger <matheo.leger@ynov.com>
2024-02-16 23:25:45 +0100

    :world_map: continue room_1 + add text for doors

--- Scripts/room_1.gd
+++ Scripts/room_1.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rotation.y = deg_to_rad(90)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
