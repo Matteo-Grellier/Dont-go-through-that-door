5c092f72ef80eb198a83d3b196994f245c60c6e1
LBROCHARD <lewis.brochard@gmail.com>
2024-02-17 00:04:16 +0100

    :sparkles: fixed light and added time before switching on

--- Scripts/light.gd
+++ Scripts/light.gd
@@ -1,10 +1,16 @@
+class_name NeonLight
+
 extends Node3D
 
-@onready var spots: Array[SpotLight3D] = [$light/OmniLight3D, $light/OmniLight3D3, $light/OmniLight3D5, $light/OmniLight3D7]
+signal light_is_on
+
 @onready var omnis: Array[OmniLight3D] = [$light/OmniLight3D2, $light/OmniLight3D4, $light/OmniLight3D9, $light/OmniLight3D6, $light/OmniLight3D8, $light/OmniLight3D10]
 
-func swhitch_lights_on():
-	for spot: SpotLight3D in spots:
-		spot.light_energy = 5
+func swhitch_lights_on(delay: float):
+	await wait(delay)
 	for omni: OmniLight3D in omnis:
 		omni.light_energy = 1
+	light_is_on.emit()
+
+func wait(duration):  
+	await get_tree().create_timer(duration, false, false, true).timeout
