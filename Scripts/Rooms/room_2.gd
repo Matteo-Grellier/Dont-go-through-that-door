7395dc68b795facb1995b848577e7e31c7b9f3ec
LBROCHARD <lewis.brochard@gmail.com>
2024-02-17 01:45:00 +0100

    :sparkles: room2

---
+++ Scripts/Rooms/room_2.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+@onready var light_back: NeonLight = $Light
+@onready var light_back2: NeonLight = $Light3
+@onready var light_front: NeonLight = $Light2
+@onready var light_front2: NeonLight = $Light4
+@onready var speaker = $Speaker
+
+func _ready():
+	start_room()
+
+func start_room():
+	light_front.swhitch_lights_on(2)
+	light_front2.swhitch_lights_on(2)
+	light_back.swhitch_lights_on(4)
+	light_back2.swhitch_lights_on(4)
+
+
+func _on_light_light_is_on():
+	await wait(1)
+	speaker.play_audio(0)
+
+
+func wait(duration):  
+	await get_tree().create_timer(duration, false, false, true).timeout
