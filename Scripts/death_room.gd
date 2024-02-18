dea7a863de644d6ac01e9944598029dd281adcc2
matheoleger <matheo.leger@ynov.com>
2024-02-18 02:51:28 +0100

    :world_map: Add deathroom ending

--- Scripts/death_room.gd
+++ Scripts/death_room.gd
@@ -1,15 +1,19 @@
 extends Node3D
 
 @onready var spot_light: SpotLight3D = $SpotLight3D
-
-
-func start_death_room():
-	await wait(2.0)
+@onready var game_manager: GameManager = get_node("/root/GameManager")
+var monster = preload("res://Scenes/monster.tscn")
+func start_room():
+	$AudioStreamPlayer.playing = true
+	await wait(3.0)
 	spot_light.light_energy = 5
+	var monster_instance = monster.instantiate()
+	monster_instance.position = Vector3(monster_instance.position.x, monster_instance.position.y+1, monster_instance.position.z-9)
+	add_child(monster_instance)
 	# Play weird sound
-	await wait(6)
+	await wait(9)
 	# Respawn
-
+	game_manager.player.position = Vector3(0, 2, 0)
 
 func wait(duration):  
 	await get_tree().create_timer(duration, false, false, true).timeout
